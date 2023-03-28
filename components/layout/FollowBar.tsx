09a00879098a80dd2ca974b12eeccf754645943f
rwxdan <rwxdan@gmail.com>
2023-03-28 13:10:20 -0400

    Create FollowBar component

---
+++ components/layout/FollowBar.tsx
@@ -0,0 +1,12 @@
+const FollowBar = () => {
+  return (
+    <div className="hidden px-6 py-4 lg:block">
+      <div className="rounded-xl bg-neutral-800 p-4 ">
+        <h2 className="text-xl font-semibold text-white">Who to follow</h2>
+        <div className="mt-4 flex flex-col gap-6">{/* TODO */}</div>
+      </div>
+    </div>
+  );
+};
+
+export default FollowBar;
