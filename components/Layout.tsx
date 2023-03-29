57071e882eeba89b30ac1a5907090940c84d82de
rwxdan <rwxdan@gmail.com>
2023-03-28 21:12:46 -0400

    Add favicon.png

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head';
 import FollowBar from './layout/FollowBar';
 import Sidebar from './layout/Sidebar';
 
@@ -7,15 +8,21 @@ interface LayoutProps {
 
 const Layout: React.FC<LayoutProps> = ({ children }) => {
   return (
-    <div className="h-screen bg-black">
-      <div className="container mx-auto h-full max-w-6xl xl:px-32">
-        <div className="grid h-full grid-cols-4">
-          <Sidebar />
-          <div className="col-span-3 border-x-[1px] border-neutral-800 lg:col-span-2">{children}</div>
-          <FollowBar />
+    <>
+      <Head>
+        <title>BlueBird</title>
+        <link rel="shortcut icon" href="/favicon.png" type="image/png" sizes="16x16" />
+      </Head>
+      <div className="h-screen bg-black">
+        <div className="container mx-auto h-full max-w-6xl xl:px-32">
+          <div className="grid h-full grid-cols-4">
+            <Sidebar />
+            <div className="col-span-3 border-x-[1px] border-neutral-800 lg:col-span-2">{children}</div>
+            <FollowBar />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
